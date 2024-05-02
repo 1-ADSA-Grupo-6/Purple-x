389840b60bd50393f1dcb51477008a29a6977182
Felippe-ASLopes <felippe.lopes@sptech.school>
2024-05-01 22:38:22 -0300

    Adição da função gerar dados aleatórios

--- Dashboard/js/dadosMaquina.js
+++ Dashboard/js/dadosMaquina.js
@@ -5,21 +5,28 @@ let idMaximo = 99
 
 // DADOS QUE VEM DO BANCO (FUTURAMENTE)
 idMaximo++ //VARIÁVEL LIMITADORA DO FOR
-let idMaq_100 = 100
+let nomeMaq_legPress = 'Leg Press'
+let categoria_LegPress = 'Pernas'
 let qtdMaq_legPress = 2
-let nomeMaq_100 = 'Leg Press'
-let categoria_100 = 'Pernas'
-let dataMediaDiario_100 = 200
-let dataMediaTempo_100 = '10:00'
+let MediaDiario_legPress = 200
+let MediaTempo_legPress = '10:00'
+let imgMaq_legPress = './img/imgMaq/legPress.png'
+
+let idMaq_100 = 100
 let ultimaManutencao_100 = '01/01/2024'
-let totalUsos_100 = 198
+let usosInicial_100 = 198
+let totalUsos_100 = usosInicial_100 
 let tempoInativo_100 = '10:00'
 let demanda_100 = 'Baixa'
-let template_musculo_100 = './img/templateMusculo/pernas.png'
-let imgMaq_100 = './img/imgMaq/legPress.png'
 let corGrafico_100 = '#7000ff'
 
 let idMaq_103 = 103
+let ultimaManutencao_103 = '01/01/2024'
+let usosInicial_103 = 190
+let totalUsos_103 = usosInicial_103 
+let tempoInativo_103 = '09:00'
+let demanda_103 = 'Baixa'
+let corGrafico_103 = '#5A00CC'
 
 // ESTRUTURA PARA O HTML
 body.innerHTML += `
@@ -27,7 +34,7 @@ body.innerHTML += `
     <div id="container_${idMaq_100}" class="um_container color2">
         <div class="info_maq">
             <div id="nome_${idMaq_100}" class="div_nome_maq text_titulo">
-                <span>${nomeMaq_100}</span>
+                <span>${nomeMaq_legPress}</span>
             </div>
             <div class="div_idMaq">
                 <span class="text_pequeno">Máquina exibida:</span>
@@ -38,16 +45,16 @@ body.innerHTML += `
                 </select>
             </div>
             <div id="img_${idMaq_100}" class="img_maquina">
-                <img src="${imgMaq_100}" id="img_leg_press">
+                <img src="${imgMaq_legPress}" id="img_leg_press">
             </div>
             <div class="dados_maq">
                 <div class="div_dados_maq text_pequeno">
                     <span>Categoria:</span>
-                    <span id="categoria_${idMaq_100}">${categoria_100}</span>
+                    <span id="categoria_${idMaq_100}">${categoria_LegPress}</span>
                 </div>
                 <div class="div_dados_maq text_pequeno">
                     <span>Quantidade de máquinas:</span>
-                    <span id="qtdMaq_${nomeMaq_100}">${qtdMaq_legPress}</span>
+                    <span id="qtdMaq_${idMaq_100}">${qtdMaq_legPress}</span>
                 </div>
                 <div class="div_dados_maq text_pequeno">
                     <span>Ultima manutenção:</span>
@@ -55,11 +62,11 @@ body.innerHTML += `
                 </div>
                 <div class="div_dados_maq text_pequeno">
                     <span>Média de uso diário:</span>
-                    <span id="mediaDiario_${idMaq_100}">${dataMediaDiario_100}</span>
+                    <span id="mediaDiario_${idMaq_100}">${MediaDiario_legPress}</span>
                 </div>
                 <div class="div_dados_maq text_pequeno">
                     <span>Média de tempo inativo:</span>
-                    <span id="mediaInativo_${idMaq_100}">${dataMediaTempo_100}</span>
+                    <span id="mediaInativo_${idMaq_100}">${MediaTempo_legPress}</span>
                 </div>
             </div>
         </div>
@@ -92,17 +99,18 @@ body.innerHTML += `
 
 // DADOS QUE VEM DO BANCO
 idMaximo++ //VARIÁVEL LIMITADORA DO FOR
-let idMaq_101 = 101
 let nomeMaq_101 = 'Supino Inclinado'
 let categoria_101 = 'Peito'
 let qtdMaq_supinoInclinado = 1
-let dataMediaDiario_101 = 500
-let dataMediaTempo_101 = '1:00'
+let MediaDiario_101 = 500
+let MediaTempo_101 = '1:00'
+
+let idMaq_101 = 101
 let ultimaManutencao_101 = '10/04/2024'
-let totalUsos_101 = 502
+let usosInicial_101 = 502
+let totalUsos_101 = usosInicial_101
 let tempoInativo_101 = '6:00'
 let demanda_101 = 'Alta'
-let template_musculo_101 = './img/templateMusculo/peito.png'
 let imgMaq_101 = './img/imgMaq/supinoInclinado.webp'
 let corGrafico_101 = '#ff9900'
 
@@ -139,21 +147,21 @@ body.innerHTML += `
                 </div>
                 <div class="div_dados_maq text_pequeno">
                     <span>Média de uso diário:</span>
-                    <span id="mediaDiario_${idMaq_101}">${dataMediaDiario_101}</span>
+                    <span id="mediaDiario_${idMaq_101}">${MediaDiario_101}</span>
                 </div>
                 <div class="div_dados_maq text_pequeno">
                     <span>Média de tempo inativo:</span>
-                    <span id="mediaInativo_${idMaq_101}">${dataMediaTempo_101}</span>
+                    <span id="mediaInativo_${idMaq_101}">${MediaTempo_101}</span>
                 </div>
             </div>
         </div>
-        <div id="index_${idMaq_101}" class="index_maq invertido">
+        <div id="index_${idMaq_101}" class="index_maq">
             <div class="container_grafico">
                 <div class="grafico_maq">
                     <canvas id="grafico_${idMaq_101}"></canvas>
                 </div>
             </div>
-            <div class="container_valor invertido">
+            <div class="container_valor">
                 <div class="div_valor text">
                     <span>Total de usos:</span>
                     <span id="total_usos_${idMaq_101}">${totalUsos_101}</span>
@@ -175,17 +183,18 @@ body.innerHTML += `
 
 // DADOS QUE VEM DO BANCO
 idMaximo++ //VARIÁVEL LIMITADORA DO FOR
-let idMaq_102 = 102
 let nomeMaq_102 = 'Smith'
 let categoria_102 = 'Pernas'
 let qtdMaq_smith = 1
-let dataMediaDiario_102 = 300
-let dataMediaTempo_102 = '14:00'
+let MediaDiario_102 = 300
+let MediaTempo_102 = '14:00'
+
+let idMaq_102 = 102
 let ultimaManutencao_102 = '15/01/2024'
-let totalUsos_102 = 342
+let usosInicial_102 = 342
+let totalUsos_102 = usosInicial_102
 let tempoInativo_102 = '16:00'
 let demanda_102 = 'Média'
-let template_musculo_102 = './img/templateMusculo/pernas.png'
 let imgMaq_102 = './img/imgMaq/smith.png'
 let corGrafico_102 = 'green'
 
@@ -222,11 +231,11 @@ body.innerHTML += `
                 </div>
                 <div class="div_dados_maq text_pequeno">
                     <span>Média de uso diário:</span>
-                    <span id="mediaDiario_${idMaq_102}">${dataMediaDiario_102}</span>
+                    <span id="mediaDiario_${idMaq_102}">${MediaDiario_102}</span>
                 </div>
                 <div class="div_dados_maq text_pequeno">
                     <span>Média de tempo inativo:</span>
-                    <span id="mediaInativo_${idMaq_102}">${dataMediaTempo_102}</span>
+                    <span id="mediaInativo_${idMaq_102}">${MediaTempo_102}</span>
                 </div>
             </div>
         </div>
--- Dashboard/js/funcGrafico.js
+++ Dashboard/js/funcGrafico.js
@@ -1,79 +1,76 @@
 function carregarGraficos() {
-    // GRÁFICO GERAL - BARRA
 
-    let graficoGeralBarra = new Chart(grafico_barra, {
-        type: 'bar',
+    let labels = [];
+    let data = [];
+
+    function gerarNumeroAleatorio() {
+
+        // GERA NÚMEROS ALEATÓRIOS, ADICIONAR UM PARA CADA MÁQUINA
+        let aleatorio_100 = Math.round(Math.random());
+        let aleatorio_101 = Math.round(Math.random());
+        let aleatorio_102 = Math.round(Math.random());
+        let aleatorio_103 = Math.round(Math.random());
+
+        // SOMA COM OS NÚMEROS ANTERIORES, ADICIONAR UM PARA CADA MÁQUINA
+        totalUsos_100 += aleatorio_100
+        totalUsos_101 += aleatorio_101
+        totalUsos_102 += aleatorio_102
+        totalUsos_103 += aleatorio_103
+
+        // PEGA O MOMENTO DA CRIAÇÃO DO DADO
+        labels.push(new Date().toLocaleTimeString())
+
+        // FORMATA O DADO PARA ATUALIZAR NO GRÁFICO
+        data.push(totalUsos_101)
+
+        // SUBSTITUIR DADOS ANTIGOS
+        if (labels.length > 10) {
+            labels.shift()
+            data.shift()
+        }
+
+        // EXECUTA A FUNÇÃO
+        atualizarGrafico()
+    }
+
+    function atualizarGrafico() {
+        graficoGeral.data.labels = labels;
+        graficoGeral.data.datasets[0].data = data;
+        graficoGeral.update();
+    }
+
+    // REPETIR A FUNÇÃO EM UM INTERVALO DE TEMPO
+    setInterval(function () {
+        gerarNumeroAleatorio()
+    }, 5000) // EM MILISSEGUNDOS
+
+    // GRÁFICO GERAL
+
+    let graficoGeral = new Chart(grafico_linha, {
+        type: 'line',
         data: {
-            labels: ['Total usos'],
+            labels: labels,
             datasets: [{
-                label: 'Peito',
-                data: [675],
-                borderWidth: 1,
-            },
-            {
-                label: 'Braço',
-                data: [421],
-                borderWidth: 1,
-            },
-            {
-                label: 'Costas',
-                data: [203],
-                borderWidth: 1,
-            },
-            {
-                label: 'Pernas',
-                data: [279],
-                borderWidth: 1,
+                label: 'teste',
+                data: data,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
             }]
         },
         options: {
             scales: {
                 y: {
-                    beginAtZero: true
+                    beginAtZero: false
                 }
             }
         }
     });
 
-    // GRÁFICO GERAL - LINHA
-
-    let graficoGeralLinha = new Chart(grafico_linha, {
-        type: 'line',
-        data: {
-            labels: ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'],
-            datasets: [{
-                label: `${nomeMaq_100}-100`,
-                data: [0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 144, 156, 168, 180, 192],
-                borderWidth: 4,
-                borderColor: corGrafico_100,
-            },
-            {
-                label: `${nomeMaq_100}-103`,
-                data: [0, 9, 34, 43, 48, 52, 59, 79, 85, 99, 108, 123, 139, 150, 165, 178, 184],
-                borderWidth: 4,
-                borderColor: 'lightblue'
-            },
-            {
-                label: nomeMaq_101,
-                data: [0, 41, 65, 80, 114, 142, 198, 219, 231, 266, 293, 342, 370, 421, 450, 487, 502],
-                borderWidth: 4,
-                borderColor: corGrafico_101
-            },
-            {
-                label: `${nomeMaq_102}-${idMaq_102}`,
-                data: [0, 12, 29, 44, 57, 66, 73, 87, 94, 100, 132, 172, 190, 241, 274, 301, 342],
-                borderWidth: 4,
-                borderColor: corGrafico_102
-            }],
-            options: {}
-        }
-    })
-
     // LEG PRESS
     let charGraph_100 = new Chart(grafico_100, {
         type: 'line',
         data: {
-            labels: ['06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'],
+            labels: labels,
             datasets: [{
                 label: idMaq_100,
                 data: [0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 144, 156, 168, 180, 192],
@@ -120,4 +117,99 @@ function carregarGraficos() {
             options: {}
         }
     })
+
+    // simulateDataCollection(graficoGeralLinha, intervaloDeAtualizacao);
+    // simulateDataCollection(charGraph_100, intervaloDeAtualizacao);
+    // simulateDataCollection(charGraph_101, intervaloDeAtualizacao);
+    // simulateDataCollection(charGraph_102, intervaloDeAtualizacao);
 }
+
+// let valor = 100
+// // Função para simular a coleta de dados
+// function simulateDataCollection(chart, interval) {
+//     setInterval(() => {
+//         // Gerar um novo valor de dados fictícios
+//         const newValue = Math.floor(Math.round(Math.random())); // Gera um valor 0 ou 1
+//         valor += newValue
+
+//         console.log(`${newValue}`)
+
+//         // Obter o tempo atual para adicionar ao rótulo de tempo
+//         let currentDate = new Date();
+//         let hours = currentDate.getHours();
+//         let minutes = currentDate.getMinutes();
+//         let seconds = currentDate.getSeconds();
+//         let timeLabel = `${hours}`;
+
+//         // Adicionar o novo valor e rótulo de tempo ao gráfico
+//         chart.data.labels.push(timeLabel);
+//         chart.data.datasets[0].data.push(valor);
+
+//         // Remover o valor mais antigo se houver mais pontos do que o limite definido
+//         if (chart.data.datasets[0].data.length > numPoints) {
+//             chart.data.datasets[0].data.shift();
+//             chart.data.labels.shift();
+//         }
+
+//         // Atualizar o gráfico
+//         chart.update();
+//     }, interval);
+// }
+
+// // Agora você pode chamar a função para simular a coleta de dados
+// const intervaloDeAtualizacao = 5000; // em milissegundos (por exemplo, a cada 5 segundos)
+// const numPoints = 1;
+
+// ---------------------------------------------------
+
+// let contador = 100;
+// let numeroAnterior = null;
+// let labels = [];
+// let data = [];
+
+// function gerarNumeroAleatorioEntreZeroEUm() {
+//   let numero = Math.round(Math.random());
+//   let numero2 = Math.round(Math.random());
+//   let numero3 = Math.round(Math.random());
+//   console.log(`${numero} | ${numero2} | ${numero3}`)
+//   contador += numero;
+//   numeroAnterior = numero;
+//   labels.push(new Date().toLocaleTimeString());
+//   data.push(contador);
+//   if (labels.length > 10) {
+//     labels.shift();
+//     data.shift();
+//   }
+//   atualizarGrafico();
+// }
+
+// function atualizarGrafico() {
+//   meuGrafico.data.labels = labels;
+//   meuGrafico.data.datasets[0].data = data;
+//   meuGrafico.update();
+// }
+
+// setInterval(function() {
+//   gerarNumeroAleatorioEntreZeroEUm();
+// }, 3000); // 5000 milissegundos = 5 segundos
+
+// var ctx = document.getElementById('grafico_linha').getContext('2d');
+// var meuGrafico = new Chart(ctx, {
+//   type: 'line',
+//   data: {
+//     labels: labels,
+//     datasets: [{
+//       label: 'Contador',
+//       data: data,
+//       borderColor: 'rgb(75, 192, 192)',
+//       tension: 0.1
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: false
+//       }
+//     }
+//   }
+// });
