b70886605330a33403ded3c0444e41f1dc8f7ee6
YasminGuiniver <yasmin.patrocinio@sptech.school>
2024-05-09 17:01:01 -0300

    alterações dash

--- Dashboard/js/funcMenu.js
+++ Dashboard/js/funcMenu.js
@@ -1,7 +1,7 @@
 function esconderMenu() {
     menu_lateral.innerHTML = `
         <div id="esconder_menu" class="div_menu_oculto" onclick="mostrarMenu()">
-        <img src="./img/icon-eye.png" class="lateral_icon">
+        <img src="./img/iconMenu-quadrado.png" class="lateral_icon">
         </div>`
     menu_lateral.classList.add('menu_oculto')
     carrocel.style.width = '95.2%'
@@ -12,30 +12,28 @@ function mostrarMenu() {
     carrocel.style.width = '100%'
     carrocel.style.marginLeft = '0%'
     menu_lateral.innerHTML = `
-        <div id="container_menu_lateral">
-            <div id="logo" class="menu">
-                <img src="./img/logo.png" class="logo">
-            </div>
+        <div id="container_menu_lateral">   
             <div id="esconder_menu" class="menu" onclick="esconderMenu()">
-                <img src="./img/icon-closedEye.png" class="lateral_icon">
+                <img src="./img/iconMenu-quadrado.png" class="lateral_icon">
             </div>
             <!-- </label> -->
             <div id="trocar_academia" class="menu">
-                <img src="./img/icon-gym.png" class="lateral_icon">
+                <img src="./img/gym-icon-white.svg" class="lateral_icon">
             </div>
             <div id="adicionar_maq" class="menu">
-                <img src="./img/icon-plus.png" class="lateral_icon">
+                <img src="./img/add-icon-white.svg" class="lateral_icon">
             </div>
             <div id="remover_maq" class="menu">
-                <img src="./img/icon-minus.png" class="lateral_icon">
+                <img src="./img/sub-icon-white.svg" class="lateral_icon">
             </div>
-            <!-- <div id="help" class="menu">
-                <img src="./img/help.png" class="lateral_icon">
-            </div> -->
+            <div id="remover_maq" class="menu">
+                <img src="./img/sino-icon-white.svg" class="lateral_icon">
+            </div>
         </div>
         <a href="../Login e Cadastro/login.html" class="menu">
         <div id="logout">
-                <img src="./img/door.png" class="lateral_icon">
+                <img src="./img/sair-icon-white.svg" class="lateral_icon">
             </div>
         </a>`
+        
 }
--- Dashboard/js/funcPopup.js
+++ Dashboard/js/funcPopup.js
@@ -1,3 +1,13 @@
+
+function abrirNotificações() {
+    let divAlerta = document.getElementById('alerta');
+    divAlerta.style.display = 'block';
+    popUp();
+    setInterval(function () {
+        popUp();
+    }, 1500);
+}
+
 function popUp() {
     let avisosLaranja_100 = document.getElementsByClassName('aviso_laranja_100');
     let avisosLaranja_101 = document.getElementsByClassName('aviso_laranja_101');
@@ -6,18 +16,29 @@ function popUp() {
     let avisosVermelho_101 = document.getElementsByClassName('aviso_vermelho_101');
     let avisosVermelho_102 = document.getElementsByClassName('aviso_vermelho_102');
 
-    hideElements(avisosLaranja_100); /*"Hidden elements" se refere a elementos HTML que estão ocultos na página da web e, portanto, não são visíveis para o usuário, aqui ela é uma função que foi definida para simplificar o código.*/
-    hideElements(avisosLaranja_101);
-    hideElements(avisosLaranja_102);
-    hideElements(avisosVermelho_100);
-    hideElements(avisosVermelho_101);
-    hideElements(avisosVermelho_102);
+    let textoAlerta = document.getElementById('alerta_texto');
 
 
+    if (demanda_100 != "Média" && demanda_100 != "Alta"
+        && demanda_101 != "Média" && demanda_101 != "Alta"
+        && demanda_102 != "Média" && demanda_102 != "Alta"
+    ) {
+        hideElements(avisosLaranja_100);
+        hideElements(avisosLaranja_101);
+        hideElements(avisosLaranja_102);
+        hideElements(avisosVermelho_100);
+        hideElements(avisosVermelho_101);
+        hideElements(avisosVermelho_102);
+        textoAlerta.style.display = 'block';
+    } else {
+        textoAlerta.style.display = 'none';
+    }
     if (demanda_100 == "Média") {
-        showElements(avisosLaranja_100); /* showElements uma função que facilite a exibição de elementos HTML de uma lista. Essa função recebe como entrada uma lista de elementos e, em seguida, itera sobre essa lista, alterando o estilo de cada elemento para torná-lo visível na página da web. */
+        showElements(avisosLaranja_100);
+        hideElements(avisosVermelho_100);
     } else if (demanda_100 == "Alta") {
         showElements(avisosVermelho_100);
+        hideElements(avisosLaranja_100);
     }
 
     if (demanda_101 == "Média") {
@@ -31,25 +52,24 @@ function popUp() {
     } else if (demanda_102 == "Alta") {
         showElements(avisosVermelho_102);
     }
-
 }
+
 
 function hideElements(elements) {
     for (let contador = 0; contador < elements.length; contador++) { /*Enquanto contador for menor que a quantidade de elementos ele vai rodar o for*/
         elements[contador].style.display = 'none'; /*no elemento x do contador vai ser aplicado o estilo x*/
     }
 }
-
 function showElements(elements) {
     for (let contador2 = 0; contador2 < elements.length; contador2++) {
         elements[contador2].style.display = 'block';
     }
 }
 
-setInterval(function () {
-    popUp();
-}, 1500);
+function fecharNotificacoes() {
+    let divAlerta = document.getElementById('alerta');
+    divAlerta.style.display = 'none';
+}
 
-/*showElements exibe os elementos
-hideElements oculta os elementos
-*/
+/* showElements uma função que facilite a exibição de elementos HTML de uma lista. Essa função recebe como entrada uma lista de elementos e, em seguida, itera sobre essa lista, alterando o estilo de cada elemento para torná-lo visível na página da web. */
+/*"Hidden elements" se refere a elementos HTML que estão ocultos na página da web e, portanto, não são visíveis para o usuário, aqui ela é uma função que foi definida para simplificar o código.*/
