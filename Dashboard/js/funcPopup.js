5880d23568b56cb2a0930eace1eeeebfeb11dbd6
jheniferlorrane <jhenifer.rodrigues@sptech.school>
2024-05-08 22:18:33 -0300

    Alerta da dashboard

--- Dashboard/js/funcPopup.js
+++ Dashboard/js/funcPopup.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Oculta todos os popups inicialmente
+function popUp() {
     document.querySelector('.alert-popup').style.display = 'none';
     document.querySelector('.error-popup').style.display = 'none';
     document.querySelector('.info-popup').style.display = 'none';
-
-    // Verifica a demanda e exibe o popup correspondente
-    if (demanda_100 == "Baixa") {
+    
+    if (demanda_100 == "Média" || demanda_101 == "Média" || demanda_102 == "Média") {
         document.querySelector('.alert-popup').style.display = 'block';
     }
-    if (demanda_100 == "Alta") {
+    if (demanda_100 == "Alta" || demanda_101 == "Alta" || demanda_102 == "Alta") {
         document.querySelector('.error-popup').style.display = 'block';
     }
-    if (demanda_100 == "Inativa") {
+    if (demanda_100 == "Inativa" || demanda_101 == "Inativa" || demanda_102 == "Inativa") {
         document.querySelector('.info-popup').style.display = 'block';
     }
-});
+}
+
+setInterval(function () {
+    popUp()
+}, 1500) 
+
